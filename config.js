2c88e8fdd8a32e8cf0de5faf6ed5f40a01b67131
tunglamgithub <107022060+tunglamgithub@users.noreply.github.com>
2022-06-07 16:47:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: '月がきれいですね',
+    desc: 'Bn có đồng ý không?',
+    btnYes: 'Có nha :3',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
